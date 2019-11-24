3a12a641de9c16b9a24427e91e96e9300e56d204
Doruk Gur <doruk@gmail.com>
2019-11-24 16:09:19 -0500

    commit before rebase

--- CCommands.v
+++ CCommands.v
@@ -11,7 +11,7 @@ Inductive CCommand : Type :=
 | CCNext: CCommand -> CCommand -> CCommand
 | CCIf: CExpression -> CCommand -> CCommand-> CCommand
 | CCWhile: CExpression -> CCommand -> CCommand
-| CCFor: CExpression -> CCommand -> CCommand
+| CCFor: CExpression-> CExpression->CExpression -> CCommand -> CCommand
 (** Not Sure About This Section
 |CCSwitch : CExpression-> CExpression -> CCommand -> CCommand
 
@@ -26,6 +26,16 @@ Notation "'IF(' exp ')' c1 'ELSE' c2" := (CCIf exp c1 c2) (at level 80, right as
 Notation "'IF(' exp ')' c1" := (CCIf exp c1 CCSkip) (at level 80, right associativity).
 Notation "'WHILE(' exp ')' c1" := (CCWhile exp c1) (at level 80, right associativity).
 Notation ";" := CCSkip.
+Notation "'FOR(' exp1 ';' exp2 ';' exp3 ')' com" := (CCFor exp1 exp2 exp3 com) (at level 80, right associativity).
+
+
+(** Notation for context and commands **)
+
+Reserved Notation 
+
+
+Inductive  CC_Eval: CCommand-> context -> context:=
+ | 
 
 
 
