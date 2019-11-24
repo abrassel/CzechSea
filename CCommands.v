0cf6f081af54497f393f186df39abfa5d82c9fa5
Doruk Gur <doruk@gmail.com>
2019-11-24 13:00:44 -0500

    add notations for initial commands

--- CCommands.v
+++ CCommands.v
@@ -1,6 +1,6 @@
-Require Import CContext
-Require Import Types
-Require Import Expressions
+Require Import CContext.
+Require Import Types.
+Require Import Expressions.
 
 
 (** Inductive Type Definitions For Commands **)
@@ -9,15 +9,23 @@ Inductive CCommand : Type :=
 | CCAssign : id -> CExpression -> CCommand
 | CCBreak: CCommand
 | CCNext: CCommand -> CCommand -> CCommand
-| CIf: CExpression -> CCommand -> CCommand-> CCommand
-| CWhile: CExpression -> CCommand -> CCommand
-| CFor: CExpression -> CCommand -> CCommand
+| CCIf: CExpression -> CCommand -> CCommand-> CCommand
+| CCWhile: CExpression -> CCommand -> CCommand
+| CCFor: CExpression -> CCommand -> CCommand
 (** Not Sure About This Section
-|CSwitch : CExpression-> CExpression -> CCommand -> CCommand
-
-
-
+|CCSwitch : CExpression-> CExpression -> CCommand -> CCommand
 
 **)
 
-                                     
+(** Added notations for commands **)                                      
+Notation "'CONTINUE'" := CCSkip.
+Notation "X '::=' exp" := (CCAssign X exp) (at level 60).
+Notation "'BREAK'" := CCBreak.
+Notation "com1 ; com2":= (CCNext com1 com2) (at level 80, right associativity).
+Notation "'IF(' exp ')' c1 'ELSE' c2" := (CCIf exp c1 c2) (at level 80, right associativity).
+Notation "'IF(' exp ')' c1" := (CCIf exp c1 CCSkip) (at level 80, right associativity).
+Notation "'WHILE(' exp ')' c1" := (CCWhile exp c1) (at level 80, right associativity).
+Notation ";" := CCSkip.
+
+
+
